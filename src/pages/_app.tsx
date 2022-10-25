adc795bae174b8cda5cbf0ee60fb9555327eff0c
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-10-25 00:05:57 -0300

    add roboto font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,10 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { Session } from "next-auth";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 function MyApp({ Component, pageProps }: AppProps<{ session: Session }>) {
   return (
