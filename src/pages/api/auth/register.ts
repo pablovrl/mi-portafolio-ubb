ad6558fc96a50ac1cce4978e5b84d5c6e10ebc52
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-10-29 15:49:28 -0300

    only accept POST method

--- src/pages/api/auth/register.ts
+++ src/pages/api/auth/register.ts
@@ -6,20 +6,27 @@ export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse
 ) {
-	const { email, password } = req.body;
+	if(req.method === 'POST') {
+		const { email, password } = req.body;
+		if(!email || !password) {
+			return res.status(400).json({message: 'Email and password are required'}); 
+		}
 
-	const user = await prisma.user.findMany({ where: { email: email } });
-	if (user.length > 0) {
-		return res.status(400).json({ error: 'User already exists' });
+		const user = await prisma.user.findMany({ where: { email: email } });
+		if (user.length > 0) {
+			return res.status(400).json({ error: 'User already exists' });
+		}
+
+		const hashedPassword = bcrypt.hashSync(password, 10);
+		const newUser = await prisma.user.create({
+			data: {
+				email: email,
+				password: hashedPassword,
+			},
+		});
+
+		return res.status(201).json(newUser);
 	}
 
-	const hashedPassword = bcrypt.hashSync(password, 10);
-	const newUser = await prisma.user.create({
-		data: {
-			email: email,
-			password: hashedPassword,
-		},
-	});
-
-	return res.status(201).json(newUser);
+	return res.status(405).send('Method not allowed');
 }
