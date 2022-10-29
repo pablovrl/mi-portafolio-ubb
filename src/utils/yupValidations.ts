8c77df777b509ba4c8fe959af9a91ccf9659f6f6
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-10-29 15:48:53 -0300

    create inputs validations to use in forms

---
+++ src/utils/yupValidations.ts
@@ -0,0 +1,21 @@
+import * as yup from 'yup';
+
+export const email = yup
+	.string()
+	.email('Ingrese un correo electrónico válido')
+	.required('Debe ingresar un correo electrónico');
+
+export const password = yup
+	.string()
+	.min(8, 'La contraseña debe tener un mínimo de 8 caracteres')
+	.required('Debe ingresar una contraseña');
+
+export const name = yup
+	.string()
+	.min(3, 'El nombre debe tener un mínimo de 3 caracteres')
+	.required('Debe ingresar un nombre');
+
+export const lastName = yup
+	.string()
+	.min(3, 'El apellido debe tener un mínimo de 3 caracteres')
+	.required('Debe ingresar un apellido');
