489a1c101686cb99099f1041d9fe50729c9ef6df
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-11-02 00:29:00 -0300

    include react-query client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,12 +5,16 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 function MyApp({ Component, pageProps }: AppProps<{ session: Session }>) {
+	const queryClient = new QueryClient();
 	return (
-		<SessionProvider session={pageProps.session}>
-			<Component {...pageProps} />
-		</SessionProvider>
+		<QueryClientProvider client={queryClient}>
+			<SessionProvider session={pageProps.session}>
+				<Component {...pageProps} />
+			</SessionProvider>
+		</QueryClientProvider>
 	);
 }
 
