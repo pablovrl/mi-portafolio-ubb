e494795170cbaa7bf76ff7d231b649bfed3db3fb
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-11-06 20:28:55 -0300

    update margin top

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -31,7 +31,7 @@ export default function SignIn() {
 			<CssBaseline />
 			<Box
 				sx={{
-					marginTop: 8,
+					marginTop: 4,
 					display: 'flex',
 					flexDirection: 'column',
 					alignItems: 'center',
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -39,7 +39,7 @@ export default function SignUp() {
 			<CssBaseline />
 			<Box
 				sx={{
-					marginTop: 8,
+					marginTop: 4,
 					display: 'flex',
 					flexDirection: 'column',
 					alignItems: 'center',
