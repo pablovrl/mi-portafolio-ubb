3bcaf2dbca032910a4ce651b9c8361977fce6a69
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-11-12 10:01:34 -0300

    add new links to navbar drawer

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,54 @@
-import { AppBar, Box, Divider, Drawer, IconButton, List, ListItemButton, ListItemText, Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, Divider, Drawer, IconButton, List, ListItemButton, ListItemText, Toolbar } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import { useState } from 'react';
 import { signOut } from 'next-auth/react';
+import Logo from './Logo';
+import Link from 'next/link';
+
+const navLinks = [
+	{ text: 'Inicio', href: '/' },
+	{ text: 'Mi portafolio', href: '/' },
+];
+
+const editLinks = [
+	{ text: 'Información Personal', href: 'edit/informacion-personal' },
+	{ text: 'Tecnologías', href: '/edit/tecnologías' },
+	{ text: 'Experiencia', href: '/edit/experiencia' },
+	{ text: 'Proyectos', href: '/edit/proyectos' }
+];
 
 const Navbar = () => {
 	const [open, setOpen] = useState(false);
 	const handleDrawerToggle = () => setOpen(!open);
+	const [isEditOpen, setEditOpen] = useState(false);
 
+	// TODO: REFACTOR THIS COMPONENT
 	const drawer = (
-		<Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
-			<Typography variant="h6" sx={{ my: 2 }}>
-				Mi Portafolio UBB
-			</Typography>
+		<Box sx={{ textAlign: 'center' }}>
+			<Box px={4} py={2}>
+				<Logo />
+			</Box>
 			<Divider />
 			<List>
+				{navLinks.map(link => (
+					<Link href={link.href} key={link.text}>
+						<ListItemButton sx={{ textAlign: 'center' }} onClick={handleDrawerToggle}>
+							<ListItemText primary={link.text} />
+						</ListItemButton>
+					</Link>
+				))}
+				<ListItemButton sx={{ textAlign: 'center' }} onClick={() => setEditOpen(!isEditOpen)}>
+					<ListItemText primary={'Editar datos'} />
+				</ListItemButton>
+				{isEditOpen && (
+					editLinks.map(link => (
+						<Link key={link.text} href={link.href}>
+							<ListItemButton sx={{ textAlign: 'center' }} onClick={handleDrawerToggle}>
+								<ListItemText primary={link.text} />
+							</ListItemButton>
+						</Link>
+					))
+				)}
 				<ListItemButton onClick={() => signOut()} sx={{ textAlign: 'center' }}>
 					<ListItemText primary={'Cerrar sesión'} />
 				</ListItemButton>
@@ -22,9 +57,9 @@ const Navbar = () => {
 	);
 
 	return (
-		<>
+		<Box mb={2}>
 			<Drawer
-				variant="temporary"
+				variant='temporary'
 				open={open}
 				onClose={handleDrawerToggle}
 				ModalProps={{
@@ -39,13 +74,12 @@ const Navbar = () => {
 			</Drawer>
 			<AppBar position='static'>
 				<Toolbar>
-					<IconButton onClick={handleDrawerToggle} color="inherit">
+					<IconButton onClick={handleDrawerToggle} color='inherit'>
 						<MenuIcon />
 					</IconButton>
-					<Typography>Mi Portafolio UBB</Typography>
 				</Toolbar>
 			</AppBar>
-		</>
+		</Box>
 	);
 };
 
