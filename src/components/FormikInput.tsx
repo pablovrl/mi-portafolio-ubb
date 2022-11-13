7dc737b2675caabfb0c39aabebfc02e05dca6cf3
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-11-13 12:22:54 -0300

    add new props

--- src/components/FormikInput.tsx
+++ src/components/FormikInput.tsx
@@ -3,15 +3,19 @@ import { useFormikContext } from 'formik';
 import { HTMLInputTypeAttribute } from 'react';
 import { CreateUser } from '../types';
 
+
 interface FormikInputProps {
 	name: string
 	label: string
 	autoFocus?: boolean
 	type?: HTMLInputTypeAttribute
 	margin?: 'none' | 'normal'
+	multiline?: boolean
+	minRows?: number
+	placeholder?: string
 }
 
-const FormikInput = ({ name, label, autoFocus, type, margin }: FormikInputProps) => {
+const FormikInput = ({ name, label, autoFocus, type, margin, multiline, minRows, placeholder }: FormikInputProps) => {
 	const context = useFormikContext<CreateUser>();
 	return (
 		<TextField
@@ -26,6 +30,9 @@ const FormikInput = ({ name, label, autoFocus, type, margin }: FormikInputProps)
 			onChange={context.handleChange}
 			error={context.touched[name] && Boolean(context.errors[name])}
 			helperText={context.touched[name] && context.errors[name]}
+			multiline={multiline}
+			minRows={minRows}
+			placeholder={placeholder}
 		/>
 	);
 };
