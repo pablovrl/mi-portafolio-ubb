195c0fbc5a87676bbd6a837862505c8b54898c8a
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-11-13 18:27:45 -0300

    create function to getSession with req and res

--- src/utils/userSession.ts
+++ src/utils/userSession.ts
@@ -1,8 +1,13 @@
-import type { GetServerSidePropsContext } from 'next';
+import type { GetServerSidePropsContext, NextApiRequest, NextApiResponse } from 'next';
 import { unstable_getServerSession } from 'next-auth/next';
 import { authOptions } from '../pages/api/auth/[...nextauth]';
 
-export async function getUserSession(context: GetServerSidePropsContext) {
+export async function getUserSession(req: NextApiRequest, res: NextApiResponse ) {
+	const session = await unstable_getServerSession(req, res, authOptions);
+	return session;
+}
+
+export async function getUserSessionWithContext(context: GetServerSidePropsContext) {
 	const session = await unstable_getServerSession(context.req, context.res, authOptions);
 	return session;
 }
