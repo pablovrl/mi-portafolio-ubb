0460cc28e3ac3760ac4c9587385ecc32a3e01de6
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-11-12 22:53:36 -0300

    update userSession to use context

--- src/utils/userSession.ts
+++ src/utils/userSession.ts
@@ -1,11 +1,8 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import type { GetServerSidePropsContext } from 'next';
 import { unstable_getServerSession } from 'next-auth/next';
 import { authOptions } from '../pages/api/auth/[...nextauth]';
 
-export async function getUserSession(
-	req: NextApiRequest,
-	res: NextApiResponse
-) {
-	const session = await unstable_getServerSession(req, res, authOptions);
+export async function getUserSession(context: GetServerSidePropsContext) {
+	const session = await unstable_getServerSession(context.req, context.res, authOptions);
 	return session;
 }
