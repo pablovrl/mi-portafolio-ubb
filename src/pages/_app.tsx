5a1c13283ed3cc4ae4ccc4df39c949d7ce80debf
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-11-14 16:00:43 -0300

    add devicon link

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,12 +6,17 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps<{ session: Session }>) {
 	const queryClient = new QueryClient();
 	return (
 		<QueryClientProvider client={queryClient}>
 			<SessionProvider session={pageProps.session}>
+				<Head>
+					<title>Mi portafolio UBB</title>
+					<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css" />
+				</Head>
 				<Component {...pageProps} />
 			</SessionProvider>
 		</QueryClientProvider>
