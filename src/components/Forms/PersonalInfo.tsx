520e2e0d3fb8f8af9ce9aca67ca31ae7448b8505
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-11-16 16:38:40 -0300

    remove formik component

--- src/components/Forms/PersonalInfo.tsx
+++ src/components/Forms/PersonalInfo.tsx
@@ -16,51 +16,39 @@ const PersonalInfo = ({ user }: PersonalInfoProps) => {
 
 	return (
 		<Box>
-			<Formik
-				initialValues={{
-					name: user.name,
-					lastName: user.lastName,
-					email: user.email,
-					about: ''
-				}}
-				onSubmit={handleFormSubmit}
-			>
-				<>
-					<Title text='Detalles personales' />
-					<Grid container spacing={2} mt={1}>
-						<Grid item xs={6}>
-							<FormikInput
-								name='name'
-								label='Nombre'
-							/>
-						</Grid>
-						<Grid item xs={6}>
-							<FormikInput
-								name='lastName'
-								label='Apellido'
-							/>
-						</Grid>
-						<Grid item xs={12}>
-							<FormikInput
-								name='email'
-								label='Correo electrónico'
-							/>
-						</Grid>
-					</Grid>
-					<Title text='Perfil' />
-					<Helptext>Los reclutadores tienen muy poco tiempo para leer sobre los postulantes, se breve. 
+			<Title text='Detalles personales' />
+			<Grid container spacing={2} mt={1}>
+				<Grid item xs={6}>
+					<FormikInput
+						name='name'
+						label='Nombre'
+					/>
+				</Grid>
+				<Grid item xs={6}>
+					<FormikInput
+						name='lastName'
+						label='Apellido'
+					/>
+				</Grid>
+				<Grid item xs={12}>
+					<FormikInput
+						name='email'
+						label='Correo electrónico'
+					/>
+				</Grid>
+			</Grid>
+			<Title text='Perfil' />
+			<Helptext>Los reclutadores tienen muy poco tiempo para leer sobre los postulantes, se breve. 
 						Escibre sobre ti y tu experiencia en 3 o 4 oraciones.</Helptext>
-					<Grid item xs={12}>
-						<FormikInput
-							name='about'
-							label='Sobre mi'
-							multiline
-							minRows={3}
-							placeholder="Ingresa una breve descripción sobre ti."
-						/>
-					</Grid>
-				</>
-			</Formik>
+			<Grid item xs={12}>
+				<FormikInput
+					name='about'
+					label='Sobre mi'
+					multiline
+					minRows={3}
+					placeholder="Ingresa una breve descripción sobre ti."
+				/>
+			</Grid>
 		</Box>
 	);
 };
