2c24e37b004db0d47e5ee1d5e8734eb4d259fef5
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-11-26 00:31:36 -0300

    add UserPortfolio interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,16 @@
+import { Contact, Experience, Project, Technology, User } from "@prisma/client"
+
 export interface CreateUser {
 	[name: string]: string,
 	lastName: string,
 	career: string,
 	email: string,
 	password: string
+}
+
+export interface UserPortfolio extends User {
+	technologies: {technology: Technology}[];
+	experience: Experience[];
+	contact: Contact[];
+	projects: Project[];
 }
