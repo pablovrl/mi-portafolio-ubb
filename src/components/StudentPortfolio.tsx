b78191d0eb351b5e21dc790dc2e99c760b339a76
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-11-26 12:32:33 -0300

    add experience and projects to the portfolio

--- src/components/StudentPortfolio.tsx
+++ src/components/StudentPortfolio.tsx
@@ -1,27 +1,39 @@
-import { Box, Grid, Typography } from '@mui/material';
+import { Box, Button, Grid, Typography } from '@mui/material';
 import { UserPortfolio } from '../types';
-import Layout from './Layout';
 import Image from 'next/image';
+import { getFormattedDate } from '../utils/getFormattedDate';
+import Link from 'next/link';
 
 interface Props {
-  user: UserPortfolio;
+	user: UserPortfolio;
 }
 
 const Title = ({ text }: { text: string }) => (
-	<Typography variant='h5' my={1} >{text}</Typography>
+	<Box width={'fit-content'} mb={2} >
+		<Typography borderBottom={'3px solid black'} variant='h5' my={1} >{text}</Typography>
+	</Box>
 );
 
 const StudentPortfolio = ({ user }: Props) => {
 	console.log(user);
 	return (
-		<Layout>
-			<Typography variant='h4' mb={1}>{user.name} {user.lastName}</Typography>
-			<Typography mb={2}>
+		<>
+			<Typography fontWeight={'bold'} variant='h4' mb={1}>{user.name} {user.lastName}</Typography>
+			<Typography>
 				{user.career === 'IECI' ? 'Ingeniería de Ejecución en Computación e Informática' : 'Ingeniería Civil Informática'}
 			</Typography>
+			<Box display="flex" gap={1}>
+				{user.contacts.map((contact) => (
+					<Box key={contact.id} mb={2}>
+						<Link href={contact.url}>
+							<a target='_blank' rel='noreferrer'> {contact.name} </a>
+						</Link>
+					</Box>
+				))}
+			</Box>
 			<Box display={'flex'} justifyContent='center'>
 				<Box position='relative' height='200px' width='200px' display={'flex'}>
-					<Image src={user.image!} layout='fill' style={{ borderRadius: '50%' }} objectFit='cover' />
+					<Image src={user.image || ''} layout='fill' style={{ borderRadius: '50%' }} objectFit='cover' />
 				</Box>
 			</Box>
 			<Title text='Sobre mí' />
@@ -29,15 +41,47 @@ const StudentPortfolio = ({ user }: Props) => {
 			<Title text='Tecnologías' />
 			<Grid container spacing={2}>
 				{user.technologies.map((tech) => (
-					<Grid item xs={6} key={tech.technology.id}>
-						<Box border={'2px solid black'} p={1} display="flex" alignItems={'center'} gap={1}>
+					<Grid item xs={4} key={tech.technology.id}>
+						<Box flexDirection={'column'} border={'2px solid #eeeee4'} borderRadius="15px" p={1} display="flex" alignItems={'center'} gap={1}>
 							<i className={tech.technology.icon} style={{ fontSize: '40px' }} />
 							<Typography variant='body1'>{tech.technology.name}</Typography>
 						</Box>
 					</Grid>
 				))}
 			</Grid>
-		</Layout>
+			<Title text='Experiencia' />
+			<Box>
+				{user.experiences.map((exp) => (
+					<Box key={exp.id} mb={2}>
+						<Typography variant='h6'>{exp.company}</Typography>
+						<Typography variant='body1'>{getFormattedDate(exp.startedAt)} - {getFormattedDate(exp.endedAt)}</Typography>
+						<Box ml={3} mt={1}>
+							<Typography fontWeight={'bold'} variant='body1'>{exp.position}</Typography>
+							<Typography textAlign='justify' variant='body1'>{exp.description}</Typography>
+						</Box>
+					</Box>
+				))}
+			</Box>
+			<Title text="Proyectos" />
+			<Box>
+				{user.projects.map((project) => (
+					<Box key={project.id} mb={2}>
+						<Typography variant='h6'>{project.name}</Typography>
+						<Box ml={3} mt={1} display="flex" flexDirection={'column'} gap={1}>
+							<Typography textAlign='justify' variant='body1'>{project.description}</Typography>
+							{project.deploy && (
+								<Link href={project.deploy} target="_blank" passHref>
+									<Button fullWidth variant="contained" color='secondary'>Ver proyecto</Button>
+								</Link>
+							)}
+							<Button variant='contained' fullWidth href={project.file}>
+								Descargar código
+							</Button>
+						</Box>
+					</Box>
+				))}
+			</Box>
+		</>
 	);
 };
 
