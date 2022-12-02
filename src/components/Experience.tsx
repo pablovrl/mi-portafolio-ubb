99178507d09239ae8a0490e0fe823aa01c7d5e16
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-12-02 17:19:08 -0300

    create Experience component

---
+++ src/components/Experience.tsx
@@ -0,0 +1,21 @@
+import { Box, Typography } from '@mui/material';
+import { getFormattedDate } from '../utils/getFormattedDate';
+
+interface ExperienceProps {
+	company: string;
+	position: string;
+	startedAt: Date;
+	endedAt: Date;
+	description: string;
+}
+
+const Experience = (props: ExperienceProps) => (
+	<Box bgcolor={'#FAFAFA'} p={5} display='flex' flexDirection={'column'} gap={1}>
+		<Typography fontWeight={'bold'}>{props.company.toUpperCase()}</Typography>
+		<Typography variant='h5' fontWeight={'bold'}>{props.position}</Typography>
+		<Typography color='grey'>{`${getFormattedDate(props.startedAt)} - ${getFormattedDate(props.endedAt)}`}</Typography>
+		<Typography color='grey' textAlign={'justify'}>{props.description}</Typography>
+	</Box>
+);
+
+export default Experience;
