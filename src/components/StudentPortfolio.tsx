38214468645cce30b0d75ec4c8b8ec4b57b8b733
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-12-02 17:20:01 -0300

    display all the user data

--- src/components/StudentPortfolio.tsx
+++ src/components/StudentPortfolio.tsx
@@ -1,86 +1,119 @@
-import { Box, Button, Grid, Typography } from '@mui/material';
+import { Box, Button, Grid, Typography, Link as MUILink } from '@mui/material';
 import { UserPortfolio } from '../types';
 import Image from 'next/image';
-import { getFormattedDate } from '../utils/getFormattedDate';
-import Link from 'next/link';
+import Project from './Project';
+import React from 'react';
+import Experience from './Experience';
 
-interface Props {
-	user: UserPortfolio;
-}
+const IMAGE_SIZE = '120px';
 
-const Title = ({ text }: { text: string }) => (
-	<Box width={'fit-content'} mb={2} >
-		<Typography borderBottom={'3px solid black'} variant='h5' my={1} >{text}</Typography>
-	</Box>
+const Title = ({children}: {children: React.ReactNode}) => (
+	<Typography fontWeight={'bold'} variant='h5' fontFamily={'monospace'}>{children}</Typography>
 );
 
-const StudentPortfolio = ({ user }: Props) => {
+const StudentPortfolio = ({ user }: {user: UserPortfolio}) => {
 	return (
-		<>
-			<Typography fontWeight={'bold'} variant='h4' mb={1}>{user.name} {user.lastName}</Typography>
-			<Typography>
-				{user.career === 'IECI' ? 'Ingeniería de Ejecución en Computación e Informática' : 'Ingeniería Civil Informática'}
-			</Typography>
-			<Box display="flex" gap={1}>
-				{user.contacts.map((contact) => (
-					<Box key={contact.id} mb={2}>
-						<Link href={contact.url}>
-							<a target='_blank' rel='noreferrer'> {contact.name} </a>
-						</Link>
+		<Box>
+			<Box>
+				<Box display={'flex'} justifyContent='space-between' flexDirection='row' gap={3} alignItems='center'>
+					<Box display='flex' flexDirection={'column'} gap={2}>
+						<Typography fontWeight={'bold'} variant='h3' mb={1}>{user.name} {user.lastName}</Typography>
+						<Typography variant='h5'>
+							{user.career === 'IECI' ? 'Ingeniería de Ejecución en Computación e Informática' : 'Ingeniería Civil Informática'}
+						</Typography>
+						<Box display={{xs: 'none', md: 'block'}}>
+							{user.contacts.map(contact => (
+								<Box key={contact.id}>
+									<MUILink href={contact.url} fontSize={'20px'} target='_blank'>
+										{contact.name}
+									</MUILink>
+								</Box>
+							))}
+						</Box>
 					</Box>
-				))}
-			</Box>
-			<Box display={'flex'} justifyContent='center'>
-				<Box position='relative' height='200px' width='200px' display={'flex'}>
-					<Image src={user.image || ''} layout='fill' style={{ borderRadius: '50%' }} objectFit='cover' />
+					<Box position='relative' height={'180px'} width={'180px'} display={{xs: 'none', md: 'flex'}}>
+						<Image src={user.image || ''} layout='fill' style={{ borderRadius: '50%' }} objectFit='cover' />
+					</Box>
+				</Box>
+				<Box display={'flex'} flexDirection='column' gap={2} mt={{xs: 2, md: 0}}>
+					<Box display={'flex'} flexDirection='column' gap={2}>
+						<Title>SOBRE MÍ</Title>
+						<Typography textAlign={'justify'}>
+							{user.about}
+						</Typography>
+					</Box>
+					<Box display={{xs: 'flex', md: 'none'}} gap={2} alignItems='center' flexDirection={'row'}>
+						<Box position='relative' height={IMAGE_SIZE} width={IMAGE_SIZE} display={'flex'}>
+							<Image src={user.image || ''} layout='fill' style={{ borderRadius: '50%' }} objectFit='cover' />
+						</Box>
+						{user.contacts.map(contact => (
+							<Box key={contact.id}>
+								<MUILink href={contact.url} fontSize={'20px'} target='_blank'>
+									{contact.name}
+								</MUILink>
+							</Box>
+						))}
+					</Box>
+					<Box display='flex' flexDirection={'column'} gap={2}>
+						<Title>TECNOLOGÍAS</Title>
+						<Grid container spacing={2} >
+							{user.technologies.map(technology => (
+								<Grid 
+									item
+									xs={4}
+									md={2}
+									key={technology.technology.id} 
+								>
+									<Box
+										bgcolor={'#FAFAFA'} 
+										display='flex'
+										flexDirection={'column'}
+										gap={1}
+										p={4}
+										alignItems='center'
+									>
+										<i style={{fontSize: '40px'}} className={technology.technology.icon} />
+										<Typography>{technology.technology.name.toUpperCase()}</Typography>
+									</Box>
+								</Grid>
+							))}
+						</Grid>
+					</Box>
+					<Box display={'flex'} flexDirection='column' gap={2}>
+						<Title>PROYECTOS</Title>
+						<Grid container spacing={2}>
+							{user.projects.map(project => (
+								<Grid key={project.id} item xs={12} md={6}> 
+									<Project 
+										name={project.name} 
+										description={project.description} 
+										link={project.file}
+										technology={project.technology}
+										course={project.course}
+									/>
+								</Grid>
+							))}
+						</Grid>
+					</Box>
+					<Box display={'flex'} flexDirection='column' gap={2}>
+						<Title>EXPERIENCIA</Title>
+						<Grid container spacing={2}>
+							{user.experiences.map(exp => (
+								<Grid item key={exp.id} xs={12} md={6}>
+									<Experience
+										company={exp.company}
+										position={exp.position}
+										startedAt={exp.startedAt}
+										endedAt={exp.endedAt}
+										description={exp.description}
+									/>
+								</Grid>
+							))}
+						</Grid>
+					</Box>
 				</Box>
 			</Box>
-			<Title text='Sobre mí' />
-			<Typography textAlign={'justify'} variant='body1'>{user.about}</Typography>
-			<Title text='Tecnologías' />
-			<Grid container spacing={2}>
-				{user.technologies.map((tech) => (
-					<Grid item xs={4} key={tech.technology.id}>
-						<Box flexDirection={'column'} border={'2px solid #eeeee4'} borderRadius="15px" p={1} display="flex" alignItems={'center'} gap={1}>
-							<i className={tech.technology.icon} style={{ fontSize: '40px' }} />
-							<Typography variant='body1'>{tech.technology.name}</Typography>
-						</Box>
-					</Grid>
-				))}
-			</Grid>
-			<Title text='Experiencia' />
-			<Box>
-				{user.experiences.map((exp) => (
-					<Box key={exp.id} mb={2}>
-						<Typography variant='h6'>{exp.company}</Typography>
-						<Typography variant='body1'>{getFormattedDate(exp.startedAt)} - {getFormattedDate(exp.endedAt)}</Typography>
-						<Box ml={3} mt={1}>
-							<Typography fontWeight={'bold'} variant='body1'>{exp.position}</Typography>
-							<Typography textAlign='justify' variant='body1'>{exp.description}</Typography>
-						</Box>
-					</Box>
-				))}
-			</Box>
-			<Title text="Proyectos" />
-			<Box>
-				{user.projects.map((project) => (
-					<Box key={project.id} mb={2}>
-						<Typography variant='h6'>{project.name}</Typography>
-						<Box ml={3} mt={1} display="flex" flexDirection={'column'} gap={1}>
-							<Typography textAlign='justify' variant='body1'>{project.description}</Typography>
-							{project.deploy && (
-								<Link href={project.deploy} target="_blank" passHref>
-									<Button fullWidth variant="contained" color='secondary'>Ver proyecto</Button>
-								</Link>
-							)}
-							<Button variant='contained' fullWidth href={project.file}>
-								Descargar código
-							</Button>
-						</Box>
-					</Box>
-				))}
-			</Box>
-		</>
+		</Box>
 	);
 };
 
