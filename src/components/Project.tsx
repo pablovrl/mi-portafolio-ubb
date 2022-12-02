24f8ad60b13efdedfc064aa5d7eb0c93405145e5
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-12-02 17:19:19 -0300

    create Project components

---
+++ src/components/Project.tsx
@@ -0,0 +1,23 @@
+import { Box, Button, Typography } from '@mui/material';
+
+interface ProjectProps {
+	technology: string;
+	name: string;
+	description: string;
+	link: string;
+	course: string;
+}
+
+const Project = (props: ProjectProps) => {
+	return (
+		<Box bgcolor={'#FAFAFA'} p={5} display='flex' flexDirection={'column'} gap={1}>
+			<Typography fontWeight={'bold'}>{props.technology.toUpperCase()}</Typography>
+			<Typography variant='h4' fontWeight={'bold'}>{props.name.toUpperCase()}</Typography>
+			<Typography fontSize={'15px'} color='grey' fontWeight={'bold'}>{props.course.toUpperCase()}</Typography>
+			<Typography color='grey' textAlign={'justify'}>{props.description}</Typography>
+			<Button href={props.link} variant='contained'>Descargar proyecto</Button>
+		</Box>
+	);
+};
+
+export default Project;
