52674ca7c2f553cd5e7399575524b5b230601dd9
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-12-06 22:09:23 -0300

    use ProfileImage component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import type { NextPage } from 'next';
-import Image from 'next/image';
 import { requireAuth } from '../utils/requireAuth';
 import Layout from '../components/Layout';
 import { prisma } from '../utils/db';
 import { User } from '@prisma/client';
 import { Box, Grid, Typography } from '@mui/material';
 import Link from 'next/link';
+import ProfileImage from '../components/ProfileImage';
 
 export const getServerSideProps = requireAuth(async () => {
 	const users = await prisma.user.findMany({ where: { portfolio: true } });
@@ -20,11 +20,9 @@ const UserCard = ({ user }: { user: User }) => (
 		<Box bgcolor={'#FAFAFA'} p={5} display='flex' gap={2} flexDirection='column' sx={{ transition: 'transform .2s', '&:hover': { transform: 'scale(1.05)' }, cursor: 'pointer' }}>
 			<Box display='flex' gap={2} alignItems='center'>
 				<Box>
-					<Typography variant='h4' fontWeight={'bold'}>{user.name.toUpperCase()} {user.lastName.toUpperCase()}</Typography>
+					<Typography variant={'h6'} fontWeight={'bold'}>{user.name.toUpperCase()} {user.lastName.toUpperCase()}</Typography>
 				</Box>
-				<Box position='relative' height={'120px'} width={{ xs: '120px', md: '100%' }} display={'flex'}>
-					<Image src={user.image || ''} layout='fill' style={{ borderRadius: '50%' }} objectFit='cover' />
-				</Box>
+				<ProfileImage src={user.image!} size={'120px'} />
 			</Box>
 			<Typography fontWeight={'bold'} color='grey'>
 				{user.career === 'IECI' ? 'Ingeniería de Ejecución en Computación e Informática' : 'Ingeniería Civil Informática'}
@@ -34,7 +32,6 @@ const UserCard = ({ user }: { user: User }) => (
 );
 
 const Home: NextPage<Props> = ({ users }) => {
-	console.log(users);
 	return (
 		<Layout>
 			<Grid container spacing={2}>
