32a3105c24887828fb3587932de63e3ca8ddb528
Pablo Villarroel <pablovillarroel135@gmail.com>
2023-01-08 00:06:05 -0300

    add text that show experience type

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,4 +1,5 @@
 import { Box, Typography } from '@mui/material';
+import { ExperienceType } from '@prisma/client';
 import { getFormattedDate } from '../utils/getFormattedDate';
 
 interface ExperienceProps {
@@ -7,13 +8,17 @@ interface ExperienceProps {
 	startedAt: string;
 	endedAt: string | null;
 	description: string;
+	type: ExperienceType;
 }
 
 const Experience = (props: ExperienceProps) => (
-	<Box bgcolor={'#FAFAFA'} p={{ xs: 2, md: 4}} display='flex' flexDirection={'column'} gap={1}>
-		<Typography fontWeight={'bold'}>{props.company.toUpperCase()}</Typography>
+	<Box bgcolor={'#FAFAFA'} p={{ xs: 2, md: 4 }} display='flex' flexDirection={'column'} gap={1}>
+		<Typography>
+			<Typography component={'span'} fontWeight={'bold'}>{props.company.toUpperCase() + ' '}</Typography>
+			<Typography component={'span'} color='grey' >{props.type === 'UNIVERSITY' ? '(Universidad)' : '(Externo)'}</Typography>
+		</Typography>
 		<Typography variant='h5' fontWeight={'bold'}>{props.position}</Typography>
-		{props.endedAt === null ? 
+		{props.endedAt === null ?
 			<Typography color='grey'>{`${getFormattedDate(props.startedAt)} - Actualidad`}</Typography> :
 			<Typography color='grey'>{`${getFormattedDate(props.startedAt)} - ${getFormattedDate(props.endedAt)}`}</Typography>
 		}
