a54830f95bd1e369b4d95323ba64f450b0afae16
Pablo Villarroel <pablovillarroel135@gmail.com>
2023-01-18 15:25:52 -0300

    use host env variable on tests

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,12 +1,10 @@
 import { test } from '@playwright/test';
 
-test('has login page', async ({ page }) => {
-	await page.goto('http://localhost:3000/');
-	await page.textContent('text=Iniciar sesión');
+test.beforeEach(async ({ page }) => {
+	await page.goto(process.env.NEXT_PUBLIC_HOST || 'http://localhost:3000');
 });
 
 test('user can login', async ({ page }) => {
-	await page.goto('http://localhost:3000/');
 	await page.getByLabel('Correo electrónico').fill(process.env.TEST_EMAIL || 'test@ubiobio.cl');
 	await page.getByLabel('Contraseña').fill(process.env.TEST_PASSWORD || 'testubb123');
 	await page.getByRole('button', { name: 'Iniciar sesión' }).click();
@@ -15,7 +13,6 @@ test('user can login', async ({ page }) => {
 });
 
 test('user can logout', async ({ page }) => {
-	await page.goto('http://localhost:3000/');
 	await page.getByLabel('Correo electrónico').fill(process.env.TEST_EMAIL || 'test@ubiobio.cl');
 	await page.getByLabel('Contraseña').fill(process.env.TEST_PASSWORD || 'testubb123');
 	await page.getByRole('button', { name: 'Iniciar sesión' }).click();
